63f8f3005cc790eb6fa64a535b04b9a31a94ffd9
Thien Truong <xayda.ln@gmail.com>
2025-08-26 18:20:50 +0700

    icon-on-background

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,27 @@ import "./app.scss";
 import Header from './components/header';
 import Form from './components/form';
 import Select from './components/select';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClover } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
-    <div className='app'>
-      <Header />
-      <Form />
-      <Select />
-    </div>
+    <>
+      <div className='app'>
+        <Header />
+        <Form />
+        <Select />
+      </div>
+
+      <div className="clover-on-background">
+        <div className="clover"><span className='c1'><FontAwesomeIcon icon={faClover} className='icon' /></span></div>
+        <div className="clover"><span className='c2'><FontAwesomeIcon icon={faClover} className='icon' /></span></div>
+        <div className="clover"><span className='c3'><FontAwesomeIcon icon={faClover} className='icon' /></span></div>
+        <div className="clover"><span className='c4'><FontAwesomeIcon icon={faClover} className='icon' /></span></div>
+        <div className="clover"><span className='c5'><FontAwesomeIcon icon={faClover} className='icon' /></span></div>
+        <div className="clover"><span className='c6'><FontAwesomeIcon icon={faClover} className='icon' /></span></div>
+      </div>
+    </>
   )
 }
 
